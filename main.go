2015d2b820c65b0084c6b9821578c53301fe76bb
Mohammed Lazhari <mohammed.lazhari@ninox.com>
2022-02-03 12:58:10 +0100

    enabled the prefork

--- main.go
+++ main.go
@@ -9,7 +9,9 @@ import (
 )
 
 func main() {
-	app := fiber.New()
+	app := fiber.New(fiber.Config{
+		Prefork: true,
+	})
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello World")
